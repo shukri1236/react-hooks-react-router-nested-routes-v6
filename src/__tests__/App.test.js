390ab63704f0bde4c456a277d3421a0e6c73cedd
shukri1236 <shukri1236>
2024-08-18 23:36:14 +0300

    done

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,6 @@
+
 test("Test passing", () => {
-    return new Promise((resolve, reject) => {
-      resolve(true);
-    });
+  return new Promise((resolve, reject) => {
+    resolve(true);
   });
+});
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+
+import { Link, useOutletContext } from "react-router-dom";
 
 function UserCard({user}) {
+  const users = useOutletContext();
+  console.log(users);
+
     return (
       <article>
           <h2>{user.name}</h2>
+          <p>
           <Link to={`/profile/${user.id}`}>View profile</Link>
+          </p>
       </article>
     );
   };
